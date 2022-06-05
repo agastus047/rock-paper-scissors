f2ce1a75590e02351219dbcdfdf22573dba45238
Agastus Joyson <agastusjoyson@gmail.com>
2022-06-05 15:15:51 +0530

    Run game within console

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rock-paper-scissors
+Play a round of Rock Paper Scissors game through the console
--- script.js
+++ script.js
@@ -11,6 +11,10 @@ function computerPlay() {
     }
 }
 
+let wins = 0;
+let loses = 0;
+let ties = 0;
+
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerSelection.toUpperCase();
     playerSelection = playerSelection.toUpperCase();
@@ -18,29 +22,36 @@ function playRound(playerSelection, computerSelection) {
         return "Wrong Input";
     }
     if (computerSelection===playerSelection) {
+        ties++;
         return "Tie";
     }
     else if (computerSelection === "ROCK") {
         if (playerSelection === "PAPER") {
+            wins ++;
             return "You Win! Paper beats Rock";
         }
         else if (playerSelection === "SCISSORS") {
+            loses ++;
             return "You Lose! Rock beats Scissors";
         }
     }
     else if (computerSelection === "PAPER") {
         if (playerSelection === "SCISSORS") {
+            wins++;
             return "You Win! Scissors beat paper";
         }
         else if (playerSelection === "ROCK") {
+            loses++;
             return "You Lose! Paper beats Rock";
         }
     }
     else if (computerSelection === "SCISSORS") {
         if (playerSelection === "ROCK") {
+            wins++;
             return "You Win! Rock beats Scissors";
         }
         else if (playerSelection === "PAPER") {
+            loses++;
             return "You Lose! Scissors beat Rock";
         }
     }
@@ -48,7 +59,14 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    let userInput = prompt("Enter Rock, Paper or Scissors");
-    let result = playRound(userInput, computerPlay());
-    console.log(result);
+    for (let i = 0; i < 5; i++) {
+        let userInput = prompt("Enter Rock, Paper or Scissors");
+        let result = playRound(userInput, computerPlay());
+        console.log(result);
+    }
+    console.log("End of Round");
+    console.log(`Wins: ${wins}\nLoses: ${loses}\nTies: ${ties}`);
+    wins = 0;
+    loses = 0;
+    ties = 0;
 }
